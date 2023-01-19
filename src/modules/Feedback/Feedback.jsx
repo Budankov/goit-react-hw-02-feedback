bd34f040ef51d8e6b5539f9a89f135cf74e372a6
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-01-19 20:53:08 +0200

    Update

---
+++ src/modules/Feedback/Feedback.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import styles from './Feedback.modules.scss';
+
+class Feedback extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Please leave feedback</h2>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
