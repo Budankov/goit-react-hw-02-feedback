189fdb558e0f92e3b580d4fd40579ccda181bbcf
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2023-01-19 22:05:17 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Feedback from './modules/Feedback/Feedback';
 
+import './sharerd/styles/styles.scss';
+
 export const App = () => {
   return (
     <>
--- src/modules/Feedback/Feedback.jsx
+++ src/modules/Feedback/Feedback.jsx
@@ -2,10 +2,50 @@ import { Component } from 'react';
 import styles from './Feedback.modules.scss';
 
 class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage(propName) {
+    const total = this.countTotalFeedback();
+    const value = this.state[propName];
+    const result = ((value / total) * 100).toFixed(2);
+    return Number(result);
+  }
+
   render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const goodParcentage = this.countPositiveFeedbackPercentage('good');
+
     return (
       <div>
-        <h2>Please leave feedback</h2>
+        <h1 className={styles.title}>Feedback Form</h1>
+        <div className={styles.wrapper}>
+          <div className={styles.block}>
+            <h2 className={styles.blockTitle}>Please leave feedback</h2>
+            <button className={styles.button}>Good</button>
+            <button className={styles.button}>Neutral</button>
+            <button className={styles.button}>Bad</button>
+          </div>
+          <div className={styles.block}>
+            <h2 className={styles.blockTitle}>Statistics</h2>
+            <p className={styles.statisitcsVote}>Good: {good}</p>
+            <p className={styles.statisitcsVote}>Neutral: {neutral}</p>
+            <p className={styles.statisitcsVote}>Bad: {bad}</p>
+            <p className={styles.statisitcsVote}>Total: {total}</p>
+            <p className={styles.statisitcsVote}>
+              Positive feedback: {goodParcentage}
+            </p>
+          </div>
+        </div>
       </div>
     );
   }
